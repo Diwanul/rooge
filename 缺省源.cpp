296940122a4d5d23d92b36e34e60b90408e8d820
Diwanul <55041545+Diwanul@users.noreply.github.com>
2019-12-28 21:16:02 +0800

    Create 缺省源.cpp

---
+++ 缺省源.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define tj
+//#define debug
+//#ifdef debug
+//#endif
+int main(){
+#ifdef tj
+	freopen(".in","r",stdin);
+	freopen(".out","w",stdout);
+#endif
+	return 0;
+}
